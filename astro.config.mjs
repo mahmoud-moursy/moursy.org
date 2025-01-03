eef7dd971bea08f8674a755d21de026197f42748
Mahmoud <mahmoud.moursy.ahmed@gmail.com>
2025-01-04 03:48:07 +0400

    Remove unneeded dep

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,13 @@ import tailwind from '@astrojs/tailwind';
 
 import cloudflare from '@astrojs/cloudflare';
 
-import htmx from 'astro-htmx';
-
 import icon from 'astro-icon';
 
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), htmx(), icon(), mdx()],
+  integrations: [tailwind(), icon(), mdx()],
   adapter: cloudflare(),
   output: 'server',
   vite: {
